bbf0c36a129ea1d2afbfcacf1f6d8363e593e1bf
Denys Bystryi <bystryydenis211999@gmail.com>
2024-09-08 21:50:51 +0300

    added reviews and cast

--- src/components/DetailsInfo/DetailsInfo.jsx
+++ src/components/DetailsInfo/DetailsInfo.jsx
@@ -3,6 +3,10 @@ import css from "./DetailsInfo.module.css";
 export default function DetailsInfo({ details }) {
   return (
     <div>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${details.poster_path}`}
+        alt={details.title}
+      />
       <h3>{details.title}</h3>
       <p>Use score: {Math.round(details.vote_average) * 10}%</p>
       <h3>Overview</h3>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from "react";
 import { getMoviesPage } from "../../service/movies-page";
 import MovieList from "../../components/MovieList/MovieList";
 import SearchForm from "../../components/SearchForm/SearchForm";
-import { useSearchParams } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 export default function MoviesPage() {
   const [movie, setMovie] = useState([]);
 
   const [params] = useSearchParams();
+
   const query = params.get("query");
+
+  const location = useLocation();
 
   useEffect(() => {
     if (!query) {
@@ -17,7 +20,6 @@ export default function MoviesPage() {
     async function getData() {
       const response = await getMoviesPage(query);
       setMovie(response);
-      console.log(response);
     }
     getData();
   }, [query]);
