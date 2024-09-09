5b67b302ec7d8eaf7b33ca022dc46bf85de78f69
Denys Bystryi <bystryydenis211999@gmail.com>
2024-09-09 18:52:23 +0300

    added errors and loaders

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,34 @@
 import { useState, useEffect } from "react";
 import { getMovies } from "../../service/trending-api";
 import { Link } from "react-router-dom";
-// import css from "./HomePage.module.css";
+import Error from "../../components/Error/Error";
+import Loader from "../../components/Error/Error";
 
 export default function HomePage() {
   const [movies, setMovies] = useState([]);
 
+  const [error, setError] = useState(false);
+  const [loader, setLoader] = useState(false);
+
   useEffect(() => {
     async function getData() {
-      const data = await getMovies();
-      setMovies(data);
+      setLoader(true);
+      try {
+        const data = await getMovies();
+        setMovies(data);
+      } catch {
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
     }
     getData();
   }, []);
   return (
     <div>
       <h1>Tranding today</h1>
+      {loader && <Loader />}
+      {error && <Error />}
       <ul>
         {movies.map((item) => (
           <li key={item.id}>
--- src/service/movies-page.js
+++ src/service/movies-page.js
@@ -14,5 +14,6 @@ export const getMoviesPage = async (query) => {
     `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${query}`,
     options
   );
+
   return response.data.results;
 };
